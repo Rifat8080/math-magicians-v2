d9d25f3c92f1f3c007b2ec2d64928f19b4a070c4
Rifat8080 <123451855+Rifat8080@users.noreply.github.com>
2023-09-15 15:28:40 +0600

    Solution updated

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import { Display, ButtonBar } from './Calculator';
+
+function App() {
+  return (
+    <>
+      <Display />
+      <ButtonBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+
+export function Display({ result }) {
+  return (
+    <div>
+      <h1>{result}</h1>
+    </div>
+  );
+}
+
+Display.defaultProps = {
+  result: '0',
+};
+
+Display.propTypes = {
+  result: PropTypes.string,
+};
+
+export function ButtonBar() {
+  return (
+    <div>
+      <div className="button">
+        <div className="row">
+          <div className="number">AC</div>
+          <div className="number">+/-</div>
+          <div className="number">%</div>
+          <div className="orange">&#247;</div>
+        </div>
+        <div className="row">
+          <div className="number">7</div>
+          <div className="number">8</div>
+          <div className="number">9</div>
+          <div className="orange">&#215;</div>
+        </div>
+        <div className="row">
+          <div className="number">4</div>
+          <div className="number">5</div>
+          <div className="number">6</div>
+          <div className="orange">-</div>
+        </div>
+        <div className="row">
+          <div className="number">1</div>
+          <div className="number">2</div>
+          <div className="number">3</div>
+          <div className="orange">+</div>
+        </div>
+        <div className="row">
+          <div className="zero">0</div>
+          <div className="decimal">.</div>
+          <div className="orange">=</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function Button({ buttonName }) {
+  return (
+    <div>
+      <h1>{buttonName}</h1>
+    </div>
+  );
+}
+
+Button.propTypes = {
+  buttonName: PropTypes.string.isRequired,
+};
